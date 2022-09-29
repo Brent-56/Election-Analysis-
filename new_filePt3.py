92aa1f8aacf90ca63b43554640f8f285fe8d49da
Brent-56 <112785655+Brent-56@users.noreply.github.com>
2022-09-29 15:48:47 -0400

    Add files via upload

---
+++ new_filePt3.py
@@ -0,0 +1,14 @@
+#what is the score
+
+score = int(input("What is your score?"))
+
+if score >= 90:
+    print("Your Score is an A")
+elif score >= 80:
+    print("Your score is a B")
+elif score >= 70:
+    print("Your score is a C")
+elif score >= 60:
+    print("Your score is a D")
+elif score <=59:
+    print("Your score is an F")
